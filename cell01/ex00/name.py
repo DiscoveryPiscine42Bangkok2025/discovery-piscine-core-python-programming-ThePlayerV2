55a7edca587913355a0f7734a8c6a4859fc55754
Astreus <teerathxd@gmail.com>
2026-04-24 03:53:19 +0000

    skibidi

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Teerath"
+last_name = "Kunariyasup"
+
+print (first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Teerath"
+last_name = "Kunariyasup"
+
+whole_name = first_name + ' ' + last_name
+
+print (whole_name)
